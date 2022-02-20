6094a525128e700b35d7df723793ee2637ea44b1
mehemken <mehemken@gmail.com>
2022-02-20 11:17:09 -0500

    no public subnets

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -3,9 +3,11 @@ variable "vpc_id" { type = string }
 variable "private_subnets" {
   type        = list(any)
   description = "The ids"
+  default     = []
 }
 
 variable "public_subnets" {
   type        = list(any)
   description = "The ids"
+  default     = []
 }
